0a834cc7675665fcce1207adf29bfdbbc3d66f33
Sem <65673890+sem1103@users.noreply.github.com>
2023-10-14 09:25:52 +0400

    Update server.js

--- server.js
+++ server.js
@@ -23,3 +23,24 @@ const httpServer = http.createServer(server);
 httpServer.listen(4004, () => {
   console.log('JSON-Server запущен на порту 4004');
 });
+
+// Таймер для отправки "keep-alive" запросов каждые 15 минут
+setInterval(() => {
+  // Создайте HTTP-клиент для отправки запроса "keep-alive"
+  const options = {
+    hostname: 'localhost', // Замените на нужный хост
+    port: 4004, // Порт вашего сервера
+    path: '/keep-alive', // Путь "keep-alive" эндпоинта
+    method: 'GET', // GET-запрос для "keep-alive"
+  };
+
+  const req = http.request(options, (res) => {
+    console.log(`Запрос "keep-alive" выполнен: ${res.statusCode}`);
+  });
+
+  req.on('error', (error) => {
+    console.error(`Ошибка при выполнении запроса "keep-alive": ${error.message}`);
+  });
+
+  req.end();
+}, 1000); 
